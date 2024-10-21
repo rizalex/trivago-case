9f9176a668b8e2a43eb94026064b96a7e9b1fc85
Rizal Ardian <12813765+rizalex@users.noreply.github.com>
2024-10-21 21:42:53 +0700

    java test

--- Fishing.java
+++ Fishing.java
@@ -1,5 +1,5 @@
 public class FishingQuote {
     public static void main(String[] args) {
-        System.out.println("There's no such thing as a bad day when you're fishing.");
+        System.out.println("There's no such thing as a bad day when you're fishing!");
     }
 }

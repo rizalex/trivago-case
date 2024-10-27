c42bd42044e3a35d547874a6ebcffca7c048365a
Rizal Ardian <12813765+rizalex@users.noreply.github.com>
2024-10-27 21:54:26 +0700

    update

--- Advice.java
+++
@@ -1,5 +0,0 @@
-public class FishingAdvice {
-    public static void main(String[] args) {
-        System.out.println("When in doubt, fish!");
-    }
-}
---
+++ app/src/main/java/org/example/Fishing.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Fishing {
+    public String getBad() {
+        return "There's no such thing as a bad day when you're fishing!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getBad());
+    }
+}
